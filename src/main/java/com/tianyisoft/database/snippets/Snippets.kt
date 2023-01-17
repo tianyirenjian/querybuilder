51caeb4fddaf6a74df9d8f69a92f9e70a8d80af8
yinyanjun <179063828@qq.com>
2023-01-17 15:33:17 +0800

    删除 deletedSnippet

--- src/main/java/com/tianyisoft/database/snippets/Snippets.kt
+++ src/main/java/com/tianyisoft/database/snippets/Snippets.kt
@@ -1,16 +1,5 @@
 package com.tianyisoft.database.snippets
 
-/**
- * @deprecated 程序支持了全局的软删除，不再需要这个
- */
-val deletedSnippet = Snippet { builder, params ->
-    if (params.isEmpty()) {
-        builder.whereNotNull("deleted_at")
-    } else {
-        builder.whereNotNull(params[0] as String)
-    }
-}
-
 val statusSnippet = Snippet { builder, params ->
     if (params.size == 1) {
         builder.where("status", "=", params[0])
